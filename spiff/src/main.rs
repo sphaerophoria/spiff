5078fb62ecc474ca8e4c8d3fc31e659ec98bcef6
Mick Sayson <mick@sayson.com>
2022-12-27 16:24:25 -0800

    Apply formatting and clippy fixes

--- spiff/src/main.rs
+++ spiff/src/main.rs
@@ -1,14 +1,15 @@
 use anyhow::{Context, Result};
 use memmap2::Mmap;
 
-use std::{
-    fs::File,
-    io::Write,
-};
+use std::{fs::File, io::Write};
 
 fn main() -> Result<()> {
-    let path1 = std::env::args().nth(1).context("Path for file 1 not provided")?;
-    let path2 = std::env::args().nth(2).context("Path for file 2 not provided")?;
+    let path1 = std::env::args()
+        .nth(1)
+        .context("Path for file 1 not provided")?;
+    let path2 = std::env::args()
+        .nth(2)
+        .context("Path for file 2 not provided")?;
 
     let file1 = File::open(path1).context("Failed to open file 1")?;
     let file2 = File::open(path2).context("Failed to open file 2")?;
@@ -24,34 +25,42 @@ fn main() -> Result<()> {
     for action in diff.actions() {
         match action {
             libdiff::DiffAction::Traverse(n) => {
-                for i in idx..idx+n {
+                for line in lines1.iter().skip(idx).take(*n) {
                     print!(" ");
-                    std::io::stdout().write_all(lines1[i]).context("Failed to write to stdout")?;
-                    print!("\n");
+                    std::io::stdout()
+                        .write_all(line)
+                        .context("Failed to write to stdout")?;
+                    println!();
                 }
-                idx = idx + n;
+                idx += n;
             }
             libdiff::DiffAction::Remove(n) => {
                 print!("{}", ansi_term::Color::Red.prefix());
-                for i in idx..idx+n {
+                for line in lines1.iter().skip(idx).take(*n) {
                     print!("-");
-                    std::io::stdout().write_all(lines1[i]).context("Failed to write to stdout")?;
-                    print!("\n");
+                    std::io::stdout()
+                        .write_all(line)
+                        .context("Failed to write to stdout")?;
+                    println!();
                 }
                 print!("{}", ansi_term::Color::Red.suffix());
-                idx = idx + n;
+                idx += n;
             }
-            libdiff::DiffAction::Insert{ starting_index, num_items} => {
+            libdiff::DiffAction::Insert {
+                starting_index,
+                num_items,
+            } => {
                 print!("{}", ansi_term::Color::Green.prefix());
-                for i in *starting_index..*starting_index+*num_items {
+                for line in lines2.iter().skip(*starting_index).take(*num_items) {
                     print!("+");
-                    std::io::stdout().write_all(lines2[i]).context("Failed to write to stdout")?;
-                    print!("\n");
+                    std::io::stdout()
+                        .write_all(line)
+                        .context("Failed to write to stdout")?;
+                    println!();
                 }
                 print!("{}", ansi_term::Color::Green.suffix());
             }
         }
-
     }
 
     Ok(())
