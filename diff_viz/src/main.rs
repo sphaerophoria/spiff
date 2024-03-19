2688f35c6a8a7cec9c88b8ee5a76a8d4aac9f8f5
Mick Sayson <mick@sayson.com>
2024-03-18 23:04:51 -0700

    :)

--- diff_viz/src/main.rs
+++ diff_viz/src/main.rs
@@ -1,8 +1,8 @@
 use eframe::{
-    egui::{self, Color32, Galley, Painter, Pos2, TextStyle, Ui},
+    egui::{self, Color32, Galley, Painter, Pos2, TextStyle, Ui, Rect},
     epaint::Stroke,
 };
-use libdiff::{DiffAlgo, DiffAlgoAction};
+use libdiff::{DiffAlgo, DiffAlgoAction, DiffAlgoDebugInfo};
 use std::sync::Arc;
 
 struct Args {
@@ -119,9 +119,9 @@ fn main() {
     let args = Args::parse(std::env::args());
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "My egui App",
+        "Diff Viz",
         native_options,
-        Box::new(|cc| Box::new(MyEguiApp::new(cc, args))),
+        Box::new(|cc| Box::new(DiffViz::new(cc, args))),
     )
     .unwrap();
 }
@@ -145,6 +145,151 @@ fn draw_elem_at_pos(elem: &Arc<Galley>, pos: Pos2, painter: &Painter) {
     painter.galley(pos, Arc::clone(elem), Color32::WHITE);
 }
 
+fn construct_diff_algo(args: &Args) -> DiffAlgo {
+    let top = args.top.unwrap_or(0) as i64;
+    let left = args.left.unwrap_or(0) as i64;
+    let right = args.right.unwrap_or(args.a.len()) as i64;
+    let bottom = args.bottom.unwrap_or(args.b.len()) as i64;
+
+    if args.backwards {
+        DiffAlgo::new_backwards(args.a.as_ref(), args.b.as_ref(), top, left, bottom, right)
+    } else if args.forgetful {
+        DiffAlgo::new_forgetful(args.a.as_ref(), args.b.as_ref(), top, left, bottom, right)
+    } else {
+        DiffAlgo::new(
+            args.a.as_ref(),
+            args.b.as_ref(),
+            top,
+            left,
+            bottom,
+            right,
+            100 * 1024 * 1024,
+        )
+        .unwrap()
+    }
+}
+
+fn add_margin_to_rect(mut rect: Rect) -> Rect {
+    const MARGIN: f32 = 30.0;
+    rect.min.x += MARGIN;
+    rect.min.y += MARGIN;
+    rect.max.x -= MARGIN;
+    rect.max.y -= MARGIN;
+    rect
+}
+
+fn draw_headers(a: &[i32], b: &[i32], grid: &Grid, ui: &Ui) {
+    let a_laid_out = lay_out_elems(a, ui);
+    let b_laid_out = lay_out_elems(b, ui);
+    let painter = ui.painter();
+    for (idx, elem) in a_laid_out.iter().enumerate() {
+        draw_elem_at_pos(elem, egui::pos2(grid.x_idx_to_pos(idx + 1), 10.0), painter);
+    }
+
+    for (idx, elem) in b_laid_out.iter().enumerate() {
+        draw_elem_at_pos(elem, egui::pos2(10.0, grid.y_idx_to_pos(idx + 1)), painter);
+    }
+
+}
+
+fn draw_grid_lines(width: usize, height: usize, grid: &Grid, painter: &Painter, stroke: Stroke) {
+    for y_idx in 0..=height {
+        let x_start = grid.x_idx_to_pos(0);
+        let x_end = grid.x_idx_to_pos(width);
+        let y = grid.y_idx_to_pos(y_idx);
+        painter.line_segment([[x_start, y].into(), [x_end, y].into()], stroke);
+    }
+
+    for x_idx in 0..=height {
+        let y_start = grid.y_idx_to_pos(0);
+        let y_end = grid.y_idx_to_pos(width);
+        let x = grid.x_idx_to_pos(x_idx);
+        painter.line_segment([[x, y_start].into(), [x, y_end].into()], stroke);
+    }
+}
+
+fn draw_diagonals(grid: &Grid, a: &[i32], b: &[i32], painter: &Painter, stroke: Stroke) {
+    for y_idx in 0..=b.len() {
+        for x_idx in 0..=a.len() {
+            let x_pos = grid.x_idx_to_pos(x_idx);
+            let y_pos = grid.y_idx_to_pos(y_idx);
+
+            if y_idx < b.len()
+                && x_idx < a.len()
+                && b.get(y_idx) == a.get(x_idx)
+            {
+                let x_end = grid.x_idx_to_pos(x_idx + 1);
+                let y_end = grid.y_idx_to_pos(y_idx + 1);
+                painter
+                    .line_segment([[x_pos, y_pos].into(), [x_end, y_end].into()], stroke);
+            }
+        }
+    }
+}
+
+const CIRCLE_RADIUS: f32 = 10.0;
+
+fn draw_dots(grid: &Grid, width: usize, height: usize, painter: &Painter, color: Color32) {
+    for y_idx in 0..=height {
+        for x_idx in 0..=width {
+            let x_pos = grid.x_idx_to_pos(x_idx);
+            let y_pos = grid.y_idx_to_pos(y_idx);
+
+            painter.circle_filled(egui::pos2(x_pos, y_pos), CIRCLE_RADIUS, color);
+        }
+    }
+}
+
+fn draw_active_dot(grid: &Grid, debug_info: &DiffAlgoDebugInfo, painter: &Painter, color: Color32) {
+    let last_step = debug_info.steps.last().and_then(|v| v.last());
+    if let Some(last_step) = last_step {
+        if last_step.0 < 0 || last_step.1 < 0 {
+            return;
+        }
+        let x_pos = grid.x_idx_to_pos(last_step.0 as usize);
+        let y_pos = grid.y_idx_to_pos(last_step.1 as usize);
+        painter.circle_filled(egui::pos2(x_pos, y_pos), CIRCLE_RADIUS, color);
+    }
+}
+
+fn draw_active_paths(grid: &Grid, debug_info: &DiffAlgoDebugInfo, painter: &Painter, stroke: Stroke) {
+    for k_steps in &debug_info.steps {
+        for step in k_steps.windows(2) {
+            if step[0].0 < 0 || step[0].1 < 0 || step[1].0 < 0 || step[1].1 < 0 {
+                continue;
+            }
+
+            let line_start_x = grid.x_idx_to_pos(step[0].0 as usize);
+            let line_start_y = grid.y_idx_to_pos(step[0].1 as usize);
+            let line_end_x = grid.x_idx_to_pos(step[1].0 as usize);
+            let line_end_y = grid.y_idx_to_pos(step[1].1 as usize);
+            let mut move_stroke = stroke;
+            move_stroke.color = Color32::GREEN;
+            painter.line_segment(
+                [
+                    [line_start_x, line_start_y].into(),
+                    [line_end_x, line_end_y].into(),
+                ],
+                move_stroke,
+            );
+        }
+    }
+}
+
+fn draw_bounding_rect(grid: &Grid, debug_info: &DiffAlgoDebugInfo, painter: &Painter, stroke: Stroke) {
+    let left_px = grid.x_idx_to_pos(debug_info.left as usize) - grid.x_spacing() / 2.0;
+    let right_px = grid.x_idx_to_pos(debug_info.right as usize) + grid.x_spacing() / 2.0;
+    let top_px = grid.y_idx_to_pos(debug_info.top as usize) - grid.y_spacing() / 2.0;
+    let bottom_px = grid.y_idx_to_pos(debug_info.bottom as usize) + grid.y_spacing() / 2.0;
+
+    let rect = egui::Rect {
+        min: egui::pos2(left_px, top_px),
+        max: egui::pos2(right_px, bottom_px),
+    };
+
+    painter.rect_stroke(rect, 3.0, stroke);
+}
+
 struct Grid {
     x_start: f32,
     x_spacing: f32,
@@ -153,6 +298,18 @@ struct Grid {
 }
 
 impl Grid {
+    fn from_rect_and_elems(rect: &Rect, a_len: usize, b_len: usize) -> Grid {
+        let x_spacing = rect.width() / a_len as f32;
+        let y_spacing = rect.height() / b_len as f32;
+
+        Grid {
+            x_spacing,
+            y_spacing,
+            x_start: rect.left(),
+            y_start: rect.top(),
+        }
+    }
+
     fn x_spacing(&self) -> f32 {
         self.x_spacing
     }
@@ -170,174 +327,60 @@ impl Grid {
     }
 }
 
-struct MyEguiApp {
+struct DiffViz {
     a: Vec<i32>,
     b: Vec<i32>,
     algo: DiffAlgo,
     finished: bool,
 }
 
-impl MyEguiApp {
+impl DiffViz {
     fn new(_cc: &eframe::CreationContext<'_>, args: Args) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        let a = args.a;
-        let b = args.b;
-        let top = args.top.unwrap_or(0) as i64;
-        let left = args.left.unwrap_or(0) as i64;
-        let right = args.right.unwrap_or(a.len()) as i64;
-        let bottom = args.bottom.unwrap_or(b.len()) as i64;
-        let algo = if args.backwards {
-            DiffAlgo::new_backwards(a.as_ref(), b.as_ref(), top, left, bottom, right)
-        } else if args.forgetful {
-            DiffAlgo::new_forgetful(a.as_ref(), b.as_ref(), top, left, bottom, right)
-        } else {
-            DiffAlgo::new(
-                a.as_ref(),
-                b.as_ref(),
-                top,
-                left,
-                bottom,
-                right,
-                100 * 1024 * 1024,
-            )
-            .unwrap()
-        };
-        MyEguiApp {
-            a,
-            b,
+        let algo = construct_diff_algo(&args);
+        DiffViz {
+            a: args.a,
+            b: args.b,
             algo,
             finished: false,
         }
     }
 }
 
-impl eframe::App for MyEguiApp {
+impl eframe::App for DiffViz {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            if ui.button("next").clicked() && !self.finished {
-                if self.algo.step(self.a.as_ref(), self.b.as_ref()) == DiffAlgoAction::Finish {
-                    self.finished = true;
-                }
+            if ui.button("next").clicked()
+                && !self.finished
+                && self.algo.step(self.a.as_ref(), self.b.as_ref()) == DiffAlgoAction::Finish
+            {
+                self.finished = true;
             }
 
-            let a_laid_out = lay_out_elems(self.a.as_ref(), ui);
-            let b_laid_out = lay_out_elems(self.b.as_ref(), ui);
 
             let available_size = ui.available_size();
-            let mut rect = ui.allocate_space(available_size).1;
-            const MARGIN: f32 = 30.0;
-            rect.min.x += MARGIN;
-            rect.min.y += MARGIN;
-            rect.max.x -= MARGIN;
-            rect.max.y -= MARGIN;
+            let rect = add_margin_to_rect(ui.allocate_space(available_size).1);
 
-            // Without the first element, spacing is width / num_elems
-            // Spacing == width / (num_elems - 1
-            //ui.allocate_space(available_size);
             let painter = ui.painter();
-            let x_spacing = rect.width() / a_laid_out.len() as f32;
-            let y_spacing = rect.height() / b_laid_out.len() as f32;
+            let grid = Grid::from_rect_and_elems(&rect, self.a.len(), self.b.len());
 
-            let grid = Grid {
-                x_spacing,
-                y_spacing,
-                x_start: rect.left(),
-                y_start: rect.top(),
-            };
-
-            for (idx, elem) in a_laid_out.iter().enumerate() {
-                draw_elem_at_pos(elem, egui::pos2(grid.x_idx_to_pos(idx + 1), 10.0), painter);
-            }
-
-            for (idx, elem) in b_laid_out.iter().enumerate() {
-                draw_elem_at_pos(elem, egui::pos2(10.0, grid.y_idx_to_pos(idx + 1)), painter);
-            }
+            draw_headers(&self.a, &self.b, &grid, ui);
 
             let color = ui.style().visuals.text_color();
             let stroke = Stroke::new(4.0, color);
-            for y_idx in 0..=self.b.len() {
-                let x_start = grid.x_idx_to_pos(0);
-                let x_end = grid.x_idx_to_pos(self.a.len());
-                let y = grid.y_idx_to_pos(y_idx);
-                painter.line_segment([[x_start, y].into(), [x_end, y].into()], stroke);
-            }
-
-            for x_idx in 0..=self.a.len() {
-                let y_start = grid.y_idx_to_pos(0);
-                let y_end = grid.y_idx_to_pos(self.b.len());
-                let x = grid.x_idx_to_pos(x_idx);
-                painter.line_segment([[x, y_start].into(), [x, y_end].into()], stroke);
-            }
+            draw_grid_lines(self.a.len(), self.b.len(), &grid, painter, stroke);
+            draw_diagonals(&grid, &self.a, &self.b, painter, stroke);
 
             let debug_info = self.algo.debug_info();
-            for y_idx in 0..=self.b.len() {
-                for x_idx in 0..=self.a.len() {
-                    let last_step = debug_info.steps.last().and_then(|v| v.last());
-                    let color = if let Some(last_step) = last_step {
-                        if y_idx == last_step.1 as usize && x_idx == last_step.0 as usize {
-                            Color32::GREEN
-                        } else {
-                            color
-                        }
-                    } else {
-                        color
-                    };
+            draw_dots(&grid, self.a.len(), self.b.len(), painter, color);
+            draw_active_dot(&grid, &debug_info, painter, Color32::GREEN);
 
-                    let x_pos = grid.x_idx_to_pos(x_idx);
-                    let y_pos = grid.y_idx_to_pos(y_idx);
+            let mut move_stroke = stroke;
+            move_stroke.color = Color32::GREEN;
+            draw_active_paths(&grid, &debug_info, painter, move_stroke);
 
-                    if y_idx < self.b.len()
-                        && x_idx < self.a.len()
-                        && self.b.get(y_idx) == self.a.get(x_idx)
-                    {
-                        let x_end = grid.x_idx_to_pos(x_idx + 1);
-                        let y_end = grid.y_idx_to_pos(y_idx + 1);
-                        painter
-                            .line_segment([[x_pos, y_pos].into(), [x_end, y_end].into()], stroke);
-                    }
-
-                    painter.circle_filled(egui::pos2(x_pos, y_pos), 10.0, color);
-                }
-            }
-
-            for k_steps in debug_info.steps {
-                for step in k_steps.windows(2) {
-                    if step[0].0 < 0 || step[0].1 < 0 || step[1].0 < 0 || step[1].1 < 0 {
-                        continue;
-                    }
-
-                    let line_start_x = grid.x_idx_to_pos(step[0].0 as usize);
-                    let line_start_y = grid.y_idx_to_pos(step[0].1 as usize);
-                    let line_end_x = grid.x_idx_to_pos(step[1].0 as usize);
-                    let line_end_y = grid.y_idx_to_pos(step[1].1 as usize);
-                    let mut move_stroke = stroke;
-                    move_stroke.color = Color32::GREEN;
-                    painter.line_segment(
-                        [
-                            [line_start_x, line_start_y].into(),
-                            [line_end_x, line_end_y].into(),
-                        ],
-                        move_stroke,
-                    );
-                }
-            }
-
-            let left_px = grid.x_idx_to_pos(debug_info.left as usize) - grid.x_spacing() / 2.0;
-            let right_px = grid.x_idx_to_pos(debug_info.right as usize) + grid.x_spacing() / 2.0;
-            let top_px = grid.y_idx_to_pos(debug_info.top as usize) - grid.y_spacing() / 2.0;
-            let bottom_px = grid.y_idx_to_pos(debug_info.bottom as usize) + grid.y_spacing() / 2.0;
-
-            let rect = egui::Rect {
-                min: egui::pos2(left_px, top_px),
-                max: egui::pos2(right_px, bottom_px),
-            };
             let mut rect_stroke = stroke;
             rect_stroke.color = Color32::from_rgba_unmultiplied(0, 255, 255, 255);
-
-            painter.rect_stroke(rect, 3.0, rect_stroke)
+            draw_bounding_rect(&grid, &debug_info, painter, rect_stroke);
         });
     }
 }
